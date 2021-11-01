626c02131aad3c237614efe4e6bdb07bd4f476e6
MacdonaldI <iain.macdonald@nrc-cnrc.gc.ca>
2021-11-01 16:14:38 -0400

    updated helper to match version in template folder

--- measures/nrc_create_geometry/resources/NRCMeasureHelper.rb
+++ measures/nrc_create_geometry/resources/NRCMeasureHelper.rb
@@ -16,7 +16,7 @@ module NRCMeasureTestHelper
     @output_folder = folder
   end
   def self.outputFolder
-    @output_folder
+    @output_folder.to_s
   end
 
   #Fancy way of getting the measure object automatically. Added check for NRC in measure name.
@@ -24,14 +24,13 @@ module NRCMeasureTestHelper
     measure_class_name = self.class.name.to_s.match((/(NRC.*)(\_Test)/i) || ((/(BTAP.*)(\_Test)/i))).captures[0]
     btap_measure = nil
     nrc_measure = nil
-	puts "#{measure_class_name}".yellow
     eval "btap_measure = #{measure_class_name}.new" if measure_class_name.to_s.include? "BTAP"
     eval "nrc_measure = #{measure_class_name}.new" if measure_class_name.to_s.include? "Nrc"
     if btap_measure.nil? and nrc_measure.nil?
       if btap_measure.nil?
         puts "Measure class #{measure_class_name} is invalid. Please ensure the test class name is of the form 'BTAPMeasureName_Test' (Note: BTAP is case sensitive.) ".red
       elsif nrc_measure.nil?
-        puts "Measure class #{measure_class_name} is invalid. Please ensure the test class name is of the form 'NRCMeasureName_Test' (Note: Nrc is case sensitive.) ".red
+        puts "Measure class #{measure_class_name} is invalid. Please ensure the test class name is of the form 'NrcMeasureName_Test' (Note: Nrc is case sensitive.) ".red
       end
       return false
     end
