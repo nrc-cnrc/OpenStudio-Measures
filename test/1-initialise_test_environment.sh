cc3148f70cd2e68fb51abaddb6aad083addd4610
MarianaNRC <37216570+MarianaNRC@users.noreply.github.com>
2021-11-02 09:45:06 -0400

    Updated the test script

--- test/1-initialise_test_environment.sh
+++ test/1-initialise_test_environment.sh
@@ -12,13 +12,13 @@ testContainer=$1
 if [ -z $testContainer ]; then
   echo -e "${YELLOW}Using default container name ${BLUE}${default_container}${NC}."
   testContainer=$default_container
-else
+  else
   echo -e "${YELLOW}Using specified container name ${BLUE}${testContainer}${NC}."
 fi
 
 # Function to create new test container.
 create_newContainer() {
-  echo -e "${GREEN}Creating docker container${NC}..."
+  echo -e "${GREEN}Creating new docker test container${NC}..."
   echo -e "${YELLOW}If you get an error here check the folder mounted in the -v option must be a sharable resource - see the docker interface to enable${NC}"
 
   echo "Creating testing container with command:"
@@ -26,16 +26,16 @@ create_newContainer() {
   MSYS_NO_PATHCONV=1 docker create -ti -P -v $shared_win_folder:/os_test --name $testContainer $image
 }
 
-# Select one of 4 options: 1) Create a new container if it doesn't exist, 2) Recreate from scratch if it exists, 3) Just update the gems, or 4) Exit.
 # The docker ps will return the container ID if it exists, nothing if not.
-echo -e "${YELLOW}Do you want to...?${NC}"
-select yn in "Container does not exist,create it" "Container exists but recreate everything from scratch" "Reinstall gems" "Cancel"; do
+ if [ ! $(docker ps -aq -f name=$testContainer) ]; then
+  echo -e "${GREEN}Container ${BLUE}$testContainer ${GREEN}doesn't exist.${NC}"
+	create_newContainer
+ else
+  echo -e "${GREEN}Found existing container ${BLUE}$testContainer. ${GREEN}Do you want to...?${NC}"
+  # Select one of 3 options: 1) Recreate from scratch if it exists, 2) Just update the gems, or 3) Exit. 
+  select yn in "Recreate everything from scratch" "Reinstall gems" "Cancel"; do
   case $yn in
-  "Container does not exist,create it")
-    create_newContainer
-    break
-    ;;
-  "Container exists but recreate everything from scratch")
+  "Recreate everything from scratch")
     if [ ! $(docker ps -aq -f status=exited -f name=$testContainer) ] || [ $(docker ps -aq -f name=$testContainer) ]; then
       docker container stop $testContainer
     fi
@@ -52,30 +52,31 @@ select yn in "Container does not exist,create it" "Container exists but recreate
     ;;
   esac
 done
-
+fi
+ 
 # Run bundle install to update the new container (this will install gems in bundle/vendor).
 echo -e "${GREEN}Starting the test environment container${NC}..."
 docker container start $testContainer
 
-# Download, install gems and run bundle.
-download_gems
+# # Download, install gems and run bundle.
+# download_gems
 
-# Install gems.
-echo -e "${GREEN}Installing gems in container: ${BLUE}$testContainer${NC}..."
-docker exec $testContainer sh -c "mkdir -p $gemDir"
-for ((iGem = 0; iGem < ${nGems}; iGem++)); do
-  echo -e "  copying ${BLUE}${server_gems[($iGem * 3)]}${NC} to ${BLUE}$gemDir${NC} in container ${BLUE}$testContainer${NC}"
-  docker cp ../.gems/${server_gems[($iGem * 3)]} $testContainer:$gemDir
-done
-echo -e "${GREEN}done${NC}."
+# # Install gems.
+# echo -e "${GREEN}Installing gems in container: ${BLUE}$testContainer${NC}..."
+# docker exec $testContainer sh -c "mkdir -p $gemDir"
+# for ((iGem = 0; iGem < ${nGems}; iGem++)); do
+  # echo -e "  copying ${BLUE}${server_gems[($iGem * 3)]}${NC} to ${BLUE}$gemDir${NC} in container ${BLUE}$testContainer${NC}"
+  # docker cp ../.gems/${server_gems[($iGem * 3)]} $testContainer:$gemDir
+# done
+# echo -e "${GREEN}done${NC}."
 
-echo -e "${GREEN}Running bundle on installed gems in container: ${BLUE}$testContainer${NC}..."
-echo -e "  ${GREEN}output in popup window${NC}"
-mintty -s 144,32 -t "Container ${testContainer} bundle log" -h always /bin/bash -c \
-"docker exec $testContainer sh -c \"cd /os_test; rm -f Gemfile.lock; bundle install; bundle list --paths; echo DONE. Press enter to close.\""
-echo -e "${GREEN}done${NC}."
+# echo -e "${GREEN}Running bundle on installed gems in container: ${BLUE}$testContainer${NC}..."
+# echo -e "  ${GREEN}output in popup window${NC}"
+# mintty -s 144,32 -t "Container ${testContainer} bundle log" -h always /bin/bash -c \
+# "docker exec $testContainer sh -c \"cd /os_test; rm -f Gemfile.lock; bundle install; bundle list --paths; echo DONE. Press enter to close.\""
+# echo -e "${GREEN}done${NC}."
 
-echo -e "${GREEN}Stopping the test environment container${NC}..."
-#docker container stop $testContainer
+# echo -e "${GREEN}Stopping the test environment container${NC}..."
+# #docker container stop $testContainer
 echo -e "...${GREEN}DONE${NC}."
 
