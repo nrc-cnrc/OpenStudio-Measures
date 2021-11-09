4dd68c207b75e74eabc30ad40c421eb8bd79961a
MarianaNRC <37216570+MarianaNRC@users.noreply.github.com>
2021-11-09 18:17:31 -0500

    Updated the server container names from '_' to '-'

--- openstudio-server/1-configure-start-server.sh
+++ openstudio-server/1-configure-start-server.sh
@@ -24,7 +24,8 @@ mintty -s 188,32 -t "OpenStudio Server Log" -h always /bin/bash -c "win_user=$(w
 download_gems
 
 # Define a container name for checking if the server is running and getting current server gemfile from.
-container=${PWD##*/}"_web_1"
+
+container=${PWD##*/}"-web-1"
 
 # Loop until container is up and running. Use 'tries' to avoid sticking here forever
 echo -e "${GREEN}Checking server is up and running${NC}..."
@@ -66,7 +67,7 @@ then
 #  *** bundle config local.openstudio-standards /var/os-gems/openstudio-standards
   echo -e "${GREEN}Recovering worker IDs from docker${NC}..."
   STEP="${GREEN}Recovering worker IDs from docker${NC}"
-  workerIDs=($(docker ps -q -f name=${PWD##*/}"_worker_"))
+  workerIDs=($(docker ps -q -f name=${PWD##*/}"-worker-"))
   echo -e "${BLUE}Worker IDs:${NC}\n$workerIDs"
 
   # Update each of the worker nodes. Need to edit the Gemfiles on each.
@@ -121,8 +122,8 @@ then
   do
     echo -e "  ${GREEN}working on ${BLUE}$file${NC}"
     dos2unix $file
-    docker exec openstudio-server_web_1 sh -c 'mkdir -p /mnt/openstudio/scripts'
-    docker cp $file openstudio-server_web_1:/mnt/openstudio/scripts/$file
+    docker exec openstudio-server-web-1 sh -c 'mkdir -p /mnt/openstudio/scripts'
+    docker cp $file openstudio-server-web-1:/mnt/openstudio/scripts/$file
   done
   echo -e "${GREEN}done${NC}."
   cd ..
