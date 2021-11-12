487bb7f179754fb3ddde8c7fad9971d484554bb3
MarianaNRC <37216570+MarianaNRC@users.noreply.github.com>
2021-11-12 14:26:40 -0500

    UpdatedNRCMeasureHelperFile

--- measures_templates/NrcTemplateMeasure/tests/test.rb
+++ measures_templates/NrcTemplateMeasure/tests/test.rb
@@ -15,82 +15,73 @@ class NrcModelMeasure_Test < Minitest::Test
 
   # Brings in helper methods to simplify argument testing of json and standard argument methods.
   include(NRCMeasureTestHelper)
-  
-  # Define the output folder.
-  @@test_dir = "#{File.expand_path(__dir__)}/output"
-  # Remove if existing found. This should only be done once.
-  if Dir.exists?(@@test_dir)
-    FileUtils.rm_rf(@@test_dir)
-	sleep 10
-  end
-  Dir.mkdir(@@test_dir)
-  
+
   def setup()
 
     @use_json_package = false
     @use_string_double = true
     @measure_interface_detailed = [
 
-        {
-            "name" => "a_string_argument",
-            "type" => "String",
-            "display_name" => "A String Argument (string)",
-            "default_value" => "The Default Value",
-            "is_required" => false
-        },
-        {
-            "name" => "a_double_argument",
-            "type" => "Double",
-            "display_name" => "A Double numeric Argument (double)",
-            "default_value" => 0,
-            "max_double_value" => 100.0,
-            "min_double_value" => 0.0,
-            "is_required" => false
-        },
-        {
-            "name" => "a_string_double_argument",
-            "type" => "StringDouble",
-            "display_name" => "A String Double numeric Argument (double)",
-            "default_value" => 23.0,
-            "max_double_value" => 100.0,
-            "min_double_value" => 0.0,
-            "valid_strings" => ["NA"],
-            "is_required" => false
-        },
-        {
-            "name" => "a_choice_argument",
-            "type" => "Choice",
-            "display_name" => "A Choice String Argument ",
-            "default_value" => "choice_1",
-            "choices" => ["choice_1", "choice_2"],
-            "is_required" => false
-        },
-        {
-            "name" => "a_bool_argument",
-            "type" => "Bool",
-            "display_name" => "A Boolean Argument ",
-            "default_value" => false,
-            "is_required" => true
-        }
+      {
+        "name" => "a_string_argument",
+        "type" => "String",
+        "display_name" => "A String Argument (string)",
+        "default_value" => "The Default Value",
+        "is_required" => false
+      },
+      {
+        "name" => "a_double_argument",
+        "type" => "Double",
+        "display_name" => "A Double numeric Argument (double)",
+        "default_value" => 0,
+        "max_double_value" => 100.0,
+        "min_double_value" => 0.0,
+        "is_required" => false
+      },
+      {
+        "name" => "a_string_double_argument",
+        "type" => "StringDouble",
+        "display_name" => "A String Double numeric Argument (double)",
+        "default_value" => 23.0,
+        "max_double_value" => 100.0,
+        "min_double_value" => 0.0,
+        "valid_strings" => ["NA"],
+        "is_required" => false
+      },
+      {
+        "name" => "a_choice_argument",
+        "type" => "Choice",
+        "display_name" => "A Choice String Argument ",
+        "default_value" => "choice_1",
+        "choices" => ["choice_1", "choice_2"],
+        "is_required" => false
+      },
+      {
+        "name" => "a_bool_argument",
+        "type" => "Bool",
+        "display_name" => "A Boolean Argument ",
+        "default_value" => false,
+        "is_required" => true
+      }
 
     ]
 
     @good_input_arguments = {
-        "a_string_argument" => "MyString",
-        "a_double_argument" => 50.0,
-        "a_string_double_argument" => "50.0",
-        "a_choice_argument" => "choice_1",
-        "a_bool_argument" => true
+      "a_string_argument" => "MyString",
+      "a_double_argument" => 50.0,
+      "a_string_double_argument" => "50.0",
+      "a_choice_argument" => "choice_1",
+      "a_bool_argument" => true
     }
 
   end
 
   def test_sample_1()
-    puts "Testing  model creation 1".blue
+    puts "Testing  model creation 1".green
     ####### Test Model Creation######
-	# Set output folder. This should be unique to avoid other tests writing to the same location.
-    NRCMeasureTestHelper.setOutputFolder("#{@@test_dir}/test_sample_1")
-	
+    # Define the output folder for this test (optional - default is the method name).
+    output_file_path = NRCMeasureTestHelper.appendOutputFolder("test_sample_1")
+
     #You'll need a seed model to test against. You have a few options.
     # If you are only testing arguments, you can use an empty model like I am doing here.
     # Option 1: Model CreationCreate Empty Model object and start doing things to it. Here I am creating an empty model
@@ -117,17 +108,15 @@ class NrcModelMeasure_Test < Minitest::Test
     # While debugging and testing, it is sometimes nice to make a copy of the model as it was.
     before_measure_model = copy_model(model)
 
-    # You can save your file anytime you want.
-    BTAP::FileIO::save_osm(model, "#{NRCMeasureTestHelper.outputFolder}/model.osm")
-
+    # save the model to test output directory
+    output_path = "#{output_file_path}/test_output.osm"
+    model.save(output_path, true)
     #We can even call the standard methods to apply to the model.
     necb2011_standard.model_add_design_days_and_weather_file(model, 'NECB HDD Method', 'CAN_BC_Vancouver.Intl.AP.718920_CWEC2016.epw')
 
     puts BTAP::FileIO.compare_osm_files(before_measure_model, model)
     necb2011_standard.apply_standard_construction_properties(model: model) # standards candidate
 
-    input_arguments = nil
-
     # Set up your argument list to test.
     input_arguments = {
       "a_string_argument" => "MyString",
@@ -138,28 +127,26 @@ class NrcModelMeasure_Test < Minitest::Test
 
     # Create an instance of the measure
     runner = run_measure(input_arguments, model)
-    puts show_output(runner.result)
+    show_output(runner.result)
 
     assert(runner.result.value.valueName == 'Success')
   end
-  
+
   def test_sample_2()
-    puts "Testing  model creation 2".blue
-	
+    puts "Testing  model creation 2".green
+
     ####### Test Model Creation######
-	# Set output folder. This should be unique to avoid other tests writing to the same location.
-    NRCMeasureTestHelper.setOutputFolder("#{@@test_dir}/test_sample_2")
-	
+    # Define the output folder for this test (optional - default is the method name).
+    NRCMeasureTestHelper.appendOutputFolder("test_sample_2")
+
     # Set standard to use.
     standard = Standard.build("NECB2017")
 
     # Create prototype model and update to follow standard rules (plus any sideload).
     model = standard.model_create_prototype_model(template: "NECB2017",
-                                                      building_type: "SmallOffice",
-                                                      epw_file: "CAN_AB_Banff.CS.711220_CWEC2016.epw",
-													  sizing_run_dir: NRCMeasureTestHelper.outputFolder)
-
-    input_arguments = nil
+                                                  building_type: "SmallOffice",
+                                                  epw_file: "CAN_AB_Banff.CS.711220_CWEC2016.epw",
+                                                  sizing_run_dir: NRCMeasureTestHelper.appendOutputFolder("test_sample_2"))
 
     # Set up your argument list to test.
     input_arguments = {
@@ -171,14 +158,16 @@ class NrcModelMeasure_Test < Minitest::Test
 
     # Create an instance of the measure
     runner = run_measure(input_arguments, model)
-    puts show_output(runner.result)
+    show_output(runner.result)
 
     assert(runner.result.value.valueName == 'Success')
   end
-  
-    # Another simple way is to Load osm file.
+
+  # Another simple way is to Load osm file.
   def test_sample_3()
-    puts "Testing  model creation 3".blue
+    puts "Testing  model creation 3".green
+    # Define the output folder for this test (optional - default is the method name).
+    NRCMeasureTestHelper.appendOutputFolder("test_sample_3")
 
     # Load osm file
     translator = OpenStudio::OSVersion::VersionTranslator.new
@@ -202,7 +191,7 @@ class NrcModelMeasure_Test < Minitest::Test
 
     # Create an instance of the measure
     runner = run_measure(input_arguments, model)
-    puts show_output(runner.result)
+    show_output(runner.result)
 
     assert(runner.result.value.valueName == 'Success')
   end
