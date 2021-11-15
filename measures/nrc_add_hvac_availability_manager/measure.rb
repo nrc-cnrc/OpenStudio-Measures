3c51e9781d0e47a65158d1ba1ad3b277ff25400b
MarianaNRC <37216570+MarianaNRC@users.noreply.github.com>
2021-11-15 17:12:26 -0500

    Updated the test for the availability_manager measure

--- measures/nrc_add_hvac_availability_manager/measure.rb
+++ measures/nrc_add_hvac_availability_manager/measure.rb
@@ -5,10 +5,10 @@ require_relative 'resources/NRCMeasureHelper'
 class NrcAddHvacAvailabilityManager < OpenStudio::Measure::ModelMeasure
 
   attr_accessor :use_json_package, :use_string_double
-  
+
   #Adds helper functions to make life a bit easier and consistent.
   include(NRCMeasureHelper)
-  
+
   # human readable name
   def name
     # Measure name should be the title case of the class name.
@@ -22,7 +22,9 @@ class NrcAddHvacAvailabilityManager < OpenStudio::Measure::ModelMeasure
 
   # human readable description of modeling approach
   def modeler_description
-    return 'Creates an availability manager and uses a outdoor air node as the sensed property.'
+    return 'Creates an availability manager and uses a outdoor air node as the sensed property.
+            The "AvailabilityManagerLowTemperatureTurnOff" turns the system off if the temperature at the sensor node is below the specified setpoint temperature. Whereas the
+            "AvailabilityManagerHighTemperatureTurnOff" turns the system off when the temperature at sensor node is higher than the specified setpoint temperature.'
   end
 
   # Use the constructor to set global variables
@@ -39,106 +41,99 @@ class NrcAddHvacAvailabilityManager < OpenStudio::Measure::ModelMeasure
     # create all the variables, validate the ranges and types you need,  and make them available in the 'run' method as a hash after
     # you run 'arguments = validate_and_get_arguments_in_hash(model, runner, user_arguments)'
     @measure_interface_detailed = [
-        {
-            "name" => "heatcool",
-            "type" => "Choice",
-            "display_name" => "Apply to",
-            "default_value" => "cooling",
-            "choices" => ["cooling", "heating"],
-            "is_required" => true
-        },
-        {
-            "name" => "setPoint",
-            "type" => "Double",
-            "display_name" => "Turn off setpoint",
-            "default_value" => 15,
-            "max_double_value" => 50.0,
-            "min_double_value" => 0.0,
-            "is_required" => true
-        }
+      {
+        "name" => "heatcool",
+        "type" => "Choice",
+        "display_name" => "Apply to",
+        "default_value" => "cooling",
+        "choices" => ["cooling", "heating"],
+        "is_required" => true
+      },
+      {
+        "name" => "setPoint",
+        "type" => "Double",
+        "display_name" => "Turn off setpoint",
+        "default_value" => 15,
+        "max_double_value" => 50.0,
+        "min_double_value" => 0.0,
+        "is_required" => true
+      }
     ]
   end
 
   # define what happens when the measure is run
   def run(model, runner, user_arguments)
-  
+
     #Runs parent run method.
     super(model, runner, user_arguments)
-	
+
     # Gets arguments from interfaced and puts them in a hash with there display name. This also does a check on ranges to
     # ensure that the values inputted are valid based on your @measure_interface array of hashes.
     arguments = validate_and_get_arguments_in_hash(model, runner, user_arguments)
-	
+
     #puts JSON.pretty_generate(arguments)
     return false if false == arguments
-	
-	# Set local variables.
-	heatcool = arguments['heatcool']
-	setPoint = arguments['setPoint']
-	puts "heatcool #{heatcool}".yellow
-	puts "setPoint #{setPoint}".yellow
-	
-	# Identify an outside node.
-	oaNode = nil
-	nodes = model.getNodes
-	if nodes.empty? then
-	  puts "No nodes!".red
-	  return
-	else
-	  nodes.each do |node|
-	  
-	    # The OA nodes have no inlet port defined, only an outlet port
-		if !node.inletModelObject.is_initialized then
-	      puts "#{node}".green
-		  oaNode = node
-		  break # Jump out of this loop when first OA node discovered.
-		end
-	  end
-	end
-	
-	#puts "Found OA node: #{oaNode.name}".yellow
-	
-	# Create the correct availability manager.
-	# The schedule for the manager defaults to always on (which is what we want).
-	# Once created attach to the correct equipment.
-	if heatcool == "cooling" then
-	  availabilityMgr = OpenStudio::Model::AvailabilityManagerLowTemperatureTurnOff.new(model)
-	  availabilityMgr.setSensorNode(oaNode)
-	  availabilityMgr.setTemperature(setPoint)
-	  #puts "#{availabilityMgr}".pink
-	  
-	  # Now loop through the cooling devices and link them to the new availability manager.
-	  chillers = model.getChillerElectricEIRs
-	  if !chillers.empty? then
-	    chillers.each do |chiller|
-		  #puts "EIR Chiller\n#{chiller}".yellow
-	      loop = chiller.plantLoop
-		  if !loop.empty? then
-		    loop.get.addAvailabilityManager(availabilityMgr)
-			availMgrs = loop.get.availabilityManagers
-			#puts "#{loop.get}".light_blue
-			#availMgrs.each do |mgr|
-			#  puts "#{mgr}".red
-			#end
-		    #puts "Updated plant loop\n#{loop.get}".pink
-		  end
-		end
-	  end
-	  coils = model.getCoilCoolingDXSingleSpeeds
-	  if !coils.empty? then
-	    coils.each do |coil|
-		  puts "Coil\n#{coil}".yellow
-		  coil.setMinimumOutdoorDryBulbTemperatureforCompressorOperation(setPoint)
-		end
-	  end
-	else # heating
-	  availabilityMgr = OpenStudio::Model::AvailabilityManagerHighTemperatureTurnOff.new(model)
-	  availabilityMgr.setSensorNode(oaNode)
-	  availabilityMgr.setTemperature(setPoint)
-	end
-	
 
-    #Do something.
+    # Set local variables.
+    heatcool = arguments['heatcool']
+    setPoint = arguments['setPoint']
+
+    # Identify an outside node.
+    oaNode = nil
+    nodes = model.getNodes
+    if nodes.empty? then
+      puts "No nodes!".red
+      return
+    else
+      nodes.each do |node|
+        # The OA nodes have no inlet port defined, only an outlet port
+        if !node.inletModelObject.is_initialized then
+          puts "#{node}".green
+          oaNode = node
+          break # Jump out of this loop when first OA node discovered.
+        end
+      end
+    end
+
+    #puts "Found OA node: #{oaNode.name}".green
+
+    # Create the correct availability manager.
+    # The schedule for the manager defaults to always on (which is what we want).
+    # Once created attach to the correct equipment.
+    if heatcool == "cooling" then
+      # The "AvailabilityManagerLowTemperatureTurnOff" turns the system off if the temperature at the sensor node is below the specified setpoint temperature.
+      availabilityMgr = OpenStudio::Model::AvailabilityManagerLowTemperatureTurnOff.new(model)
+      availabilityMgr.setSensorNode(oaNode)
+      availabilityMgr.setTemperature(setPoint)
+      # Now loop through the cooling devices and link them to the new availability manager.
+      chillers = model.getChillerElectricEIRs
+      if !chillers.empty? then
+        chillers.each do |chiller|
+          #puts "EIR Chiller\n#{chiller}".light_blue
+          loop = chiller.plantLoop
+          if !loop.empty? then
+            loop.get.addAvailabilityManager(availabilityMgr)
+            availMgrs = loop.get.availabilityManagers
+            #availMgrs.each do |mgr|
+            #  puts "#{mgr}".light_blue
+            #end
+            #puts "Updated plant loop\n#{loop.get}".pink
+          end
+        end
+      end
+      coils = model.getCoilCoolingDXSingleSpeeds
+      if !coils.empty? then
+        coils.each do |coil|
+          puts "Coil\n#{coil}".yellow
+          coil.setMinimumOutdoorDryBulbTemperatureforCompressorOperation(setPoint)
+        end
+      end
+    else
+      availabilityMgr = OpenStudio::Model::AvailabilityManagerHighTemperatureTurnOff.new(model)
+      availabilityMgr.setSensorNode(oaNode)
+      availabilityMgr.setTemperature(setPoint)
+    end
+
     return true
   end
 end
