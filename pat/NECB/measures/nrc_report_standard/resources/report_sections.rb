9ae6afe081b6de58488daecb9db1e7667b343581
Macdonald, Iain <iain.macdonald@nrc-cnrc.gc.ca>
2022-11-23 12:23:56 -0500

    automatically updated

--- pat/NECB/measures/nrc_report_standard/resources/report_sections.rb
+++ pat/NECB/measures/nrc_report_standard/resources/report_sections.rb
@@ -322,7 +322,7 @@ class NrcReportingMeasureStandard < OpenStudio::Measure::ReportingMeasure
 
       # Extract data from hash and ensure it has keys as symbols.
       data = Array.new
-      data << ["Space Name", "Space Type Name", "Lighting", "NECB 2017 Reference Lighting"]
+      data << ["Space Name", "Space Type Name", "Lighting", "NECB Reference Lighting"]
       data << [" ", " ", "W/m<sup>2</sup>", "W/m<sup>2</sup>"]
 
       btap_data[:space_table].each do |space|
@@ -331,16 +331,26 @@ class NrcReportingMeasureStandard < OpenStudio::Measure::ReportingMeasure
         building_type = space[:building_type]
         space_type_name = space[:space_type_name]
 
-        # In btap_data the building_type is added to the space type name
-        btap_data_space_type_name = "#{building_type}" + " " + "#{space_type_name}"
+        # For dwelling units they will have unique space types (see NECB2011/autozone.rb line ~250 in openstudio-standards)
+        # Hack off the number at the end of the name.
+        if space_type_name.include? "Dwelling unit"
+          space_type_name.tr("0-9", "")
+        end
 
-        # Get LPD from btap_data
+        # In btap_data the building_type is added to the space type name.
+        btap_data_space_type_name = "#{building_type}" + " " + "#{space_type_name}"
+        puts "******** #{btap_data_space_type_name}"
+
+        # Get LPD from btap_data.
         btap_data_sapce_type = btap_data[:space_type_table].detect { |s| (s['name'] == btap_data_space_type_name) }
         lighting_w_per_m_sq = btap_data_sapce_type["lighting_w_per_m_sq"]
 
-        # Get LPD from NECB 2017 Standards
+        # Get LPD from NECB.
         spacetype_data = standard.standards_data['tables']['space_types']['table']
+        File.open('./spacetype_data.json', 'w') { |f| f.write(JSON.pretty_generate(spacetype_data, allow_nan: true)) }
+        puts "Wrote file spacetype_data.json in #{Dir.pwd} "
         space_type_properties = spacetype_data.detect { |s| (s['building_type'] == building_type) && (s['space_type'] == space_type_name) }
+        
         # Convert from W/ft2 to W/m2
         necb_lighting_per_area_ft2 = space_type_properties["lighting_per_area"]
         necb_lighting_per_area = necb_lighting_per_area_ft2 * 10.76391041671
