955a61449d64776a8ffa55b45218a5d6cb3afa2a
Macdonald, Iain <iain.macdonald@nrc-cnrc.gc.ca>
2023-02-09 08:48:04 -0500

    fixing method calls and order of actions in test templates

--- measures_templates/NrcTemplateMeasure/tests/test.rb
+++ measures_templates/NrcTemplateMeasure/tests/test.rb
@@ -93,17 +93,6 @@ class NrcModelMeasure_Test < Minitest::Test
   def test_sample_A()
     puts "Testing model creation - example A".green
 
-    # Define the output folder for this test. Thi sis used as the folder name and in the test README.md file as the
-    #  section name. (Optional - default is the method name but better to use a meaningful name here).
-    output_file_path = NRCMeasureTestHelper.appendOutputFolder("Test Model Creation A")
-
-    # Load osm file.
-    model = load_test_osm("#{File.dirname(__FILE__)}/SmallOffice.osm")
-
-    # Assign the local weather file (have to provide a full path to EpwFile).
-    epw = OpenStudio::EpwFile.new("#{File.dirname(__FILE__)}/weather_files/CAN_ON_Ottawa-Macdonald-Cartier.Intl.AP.716280_CWEC2016.epw")
-    OpenStudio::Model::WeatherFile::setWeatherFile(model, epw)
-
     # Set up your argument list to test. Or use @good_input_arguments
     input_arguments = {
       "a_string_argument" => "MyString",
@@ -111,6 +100,17 @@ class NrcModelMeasure_Test < Minitest::Test
       "a_string_double_argument" => 75.3,
       "a_choice_argument" => "choice_1"
     }
+
+    # Define the output folder for this test. This is used as the folder name and in the test README.md file as the
+    #  section name. The arguments are used to store the path in a hash for when we have multiple test methods in a class.
+    output_file_path = NRCMeasureTestHelper.appendOutputFolder("Test Model Creation A", input_arguments)
+
+    # Load osm file.
+    model = load_test_osm("#{File.dirname(__FILE__)}/SmallOffice.osm")
+
+    # Assign the local weather file (have to provide a full path to EpwFile).
+    epw = OpenStudio::EpwFile.new("#{File.dirname(__FILE__)}/weather_files/CAN_ON_Ottawa-Macdonald-Cartier.Intl.AP.716280_CWEC2016.epw")
+    OpenStudio::Model::WeatherFile::setWeatherFile(model, epw)
 
     # Run the measure. This saves the updated model to "#{output_file_path}/test_output.osm".
     runner = run_measure(input_arguments, model)
@@ -130,9 +130,17 @@ class NrcModelMeasure_Test < Minitest::Test
   def test_sample_B()
     puts "Testing model creation - example B".green
 
-    # Define the output folder for this test. Thi sis used as the folder name and in the test README.md file as the
-    #  section name. (Optional - default is the method name but better to use a meaningful name here).
-    NRCMeasureTestHelper.appendOutputFolder("Test Model Creation B")
+    # Set up your argument list to test. Or use @good_input_arguments
+    input_arguments = {
+      "a_string_argument" => "MyString",
+      "a_double_argument" => 10.0,
+      "a_string_double_argument" => 75.3,
+      "a_choice_argument" => "choice_1"
+    }
+
+    # Define the output folder for this test. This is used as the folder name and in the test README.md file as the
+    #  section name. The arguments are used to store the path in a hash for when we have multiple test methods in a class.
+    output_file_path = NRCMeasureTestHelper.appendOutputFolder("Test Model Creation B", input_arguments)
 
     # Set standard to use.
     standard = Standard.build("NECB2017")
@@ -141,15 +149,8 @@ class NrcModelMeasure_Test < Minitest::Test
     model = standard.model_create_prototype_model(template: "NECB2017",
                                                   building_type: "SmallOffice",
                                                   epw_file: "CAN_AB_Banff.CS.711220_CWEC2016.epw",
-                                                  sizing_run_dir: NRCMeasureTestHelper.appendOutputFolder("test_sample_2"))
+                                                  sizing_run_dir: output_file_path)
 
-    # Set up your argument list to test. Or use @good_input_arguments
-    input_arguments = {
-      "a_string_argument" => "MyString",
-      "a_double_argument" => 10.0,
-      "a_string_double_argument" => 75.3,
-      "a_choice_argument" => "choice_1"
-    }
 
     # Run the measure. This saves the updated model to "#{output_file_path}/test_output.osm".
     runner = run_measure(input_arguments, model)
@@ -169,9 +170,17 @@ class NrcModelMeasure_Test < Minitest::Test
   def test_sample_C()
     puts "Testing model creation - example C".green
 
-    # Define the output folder for this test. Thi sis used as the folder name and in the test README.md file as the
+    # Set up your argument list to test. Or use @good_input_arguments
+    input_arguments = {
+      "a_string_argument" => "MyString",
+      "a_double_argument" => 10.0,
+      "a_string_double_argument" => 75.3,
+      "a_choice_argument" => "choice_1"
+    }
+
+    # Define the output folder for this test. This is used as the folder name and in the test README.md file as the
     #  section name. (Optional - default is the method name but better to use a meaningful name here).
-    output_file_path = NRCMeasureTestHelper.appendOutputFolder("Test Model Creation C")
+    output_file_path = NRCMeasureTestHelper.appendOutputFolder("Test Model Creation C", input_arguments)
 
     # You'll need a seed model to test against. 
     # Create an empty model and add surface geometry to the it using the BTAP wizard.
@@ -205,13 +214,6 @@ class NrcModelMeasure_Test < Minitest::Test
     # Compare the models and print to screen. Use colour coding to differentiate from other outputs on screen (green=good, yellow=warning, red=error)
     puts "#{BTAP::FileIO.compare_osm_files(before_measure_model, model)}".yellow
 
-    # Set up your argument list to test. Or use @good_input_arguments
-    input_arguments = {
-      "a_string_argument" => "MyString",
-      "a_double_argument" => 10.0,
-      "a_string_double_argument" => 75.3,
-      "a_choice_argument" => "choice_1"
-    }
 
     # Run the measure. This saves the updated model to "#{output_file_path}/test_output.osm".
     runner = run_measure(input_arguments, model)
